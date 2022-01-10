00e324db19b4868e4b290a1e9321a08959a63369
Kiwi <59049734+WillyTrek19@users.noreply.github.com>
2022-01-11 00:32:15 +0100

    Started training with maze

--- Assets/BallAgent.cs
+++ Assets/BallAgent.cs
@@ -10,18 +10,40 @@ public class BallAgent : Agent
     public Rigidbody rBody;
     public Transform Target;
 	public float forceMultiplier = 10;
+	public float time = 0.0f;
 
-	
+
+	private GameObject[] checkpoints;
+	private int checkCounter = 0;
+	private int bestCheckCounter = 0;
+	private int hit = 0;
+
+	void Start()
+    {
+		checkpoints = GameObject.FindGameObjectsWithTag("Checkpoint");
+		checkCounter = 0;
+		bestCheckCounter = 0;
+		hit = 0;
+	}
     public override void OnEpisodeBegin()
     {
-       // If the Agent fell, zero its momentum
-        if (this.transform.localPosition.y < 0)
+		// If the Agent fell, zero its momentum
+		SetReward(0.0f);
+
+		time = 0.0f;
+
+        this.rBody.angularVelocity = Vector3.zero;
+        this.rBody.velocity = Vector3.zero;
+        this.transform.localPosition = new Vector3( -15.0f, 1.0f, -35.0f );
+
+		foreach(var g in checkpoints)
         {
-            this.rBody.angularVelocity = Vector3.zero;
-            this.rBody.velocity = Vector3.zero;
-            this.transform.localPosition = new Vector3( 0, 1.0f, 0);
+			g.SetActive(true);
         }
-    }
+
+		checkCounter = 0;
+		time = 0;
+	}
 	
 	public override void CollectObservations(VectorSensor sensor)
 	{
@@ -45,27 +67,71 @@ public class BallAgent : Agent
 		// Rewards
 		float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
 
+		time += Time.deltaTime;
+
 		// Reached target
 		if (distanceToTarget < 1.42f)
 		{
-			SetReward(1.0f);
-			EndEpisode();
+			Done();
 		}
 
-		// Fell off platform
-		else if (this.transform.localPosition.y < 0)
-		{
-			EndEpisode();
-		}
+        // Fell off platform
+        else if (this.transform.localPosition.y < 0)
+        {
+			Done();
+        }
+
+		else if (time >= 10)
+        {
+			SetReward(-time);
+			Done();
+        }
 	}
 
     public void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Labyrinth")
+		float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
+
+		if (other.gameObject.CompareTag("Labyrinth"))
         {
-			Debug.Log("Collsion");
-			this.EndEpisode();
+			float reward = -10.0f + hit;
+			SetReward(reward);
+			hit++;
+			Done();
         }
+		else if (other.gameObject.CompareTag("Checkpoint"))
+        {
+			hit = 0;
+			time = 0;
+			other.gameObject.SetActive(false);
+
+			checkCounter++;
+			float reward = 1.0f;
+			reward *= checkCounter;
+			SetReward(reward);
+
+			if (bestCheckCounter < checkCounter)
+            {
+				bestCheckCounter = checkCounter;
+            }
+
+			Debug.Log("Barrier Counter: " + checkCounter
+				+ " || High Score: " + bestCheckCounter);
+        }
+    }
+
+	public void Done()
+    {
+		float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
+
+		if (distanceToTarget < 1.42f)
+		{
+			SetReward(250.0f);
+		}
+
+		Debug.Log("Distance: " + distanceToTarget);
+
+		EndEpisode();
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
